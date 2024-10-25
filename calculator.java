4a9162057fae4e5bfb5d5191939ef36da9b35018
k3shilpa <k3shilpa@gmail.com>
2024-10-25 13:48:28 +0530

    initial commit

---
+++ calculator.java
@@ -0,0 +1,15 @@
+/**
+ * This class represents a simple calculator.
+ */
+public class Calculator {
+    
+    /**
+     * Adds two integers.
+     * @param a the first integer
+     * @param b the second integer
+     * @return the sum of a and b
+     */
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
